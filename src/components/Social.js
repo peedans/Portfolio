989034cb6cff64c30e96b2f39120569f06728367
pee dansatiensakul <peezaba007@gmail.com>
2022-04-27 16:15:58 +0700

    update

--- src/components/Social.js
+++ src/components/Social.js
@@ -3,10 +3,10 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="social">
-      <a href="#" target="_blank" rel="noopender noreferrer"><i className="fab fa-github"></i></a>
-      <a href="#" target="_blank" rel="noopender noreferrer"><i className="fab fa-instagram"></i></a>
-      <a href="#" target="_blank" rel="noopender noreferrer"><i className="fab fa-facebook"></i></a>
-      <a href="#" target="_blank" rel="noopender noreferrer"><i className="fab fa-linkedin"></i></a>
+      <a href="https://github.com/peedans" target="_blank" rel="noopender noreferrer"><i className="fab fa-github"></i></a>
+      <a href="https://www.instagram.com/peedaan/?hl=en" target="_blank" rel="noopender noreferrer"><i className="fab fa-instagram"></i></a>
+      <a href="https://www.facebook.com/R4punzel/" target="_blank" rel="noopender noreferrer"><i className="fab fa-facebook"></i></a>
+      <a href="https://www.linkedin.com/in/pee-dansatiensakul-b80006233/" target="_blank" rel="noopender noreferrer"><i className="fab fa-linkedin"></i></a>
 
     </div>
   );
--- src/contents/Project.js
+++ src/contents/Project.js
@@ -9,19 +9,22 @@ const Project = () => {
             <h1 className='sectionProject'>My Project</h1>
             <div className='colmar'>
                 <h2 className='colmarHead'>Colmar Project</h2>
-                <img src={colmar} width="500" height="auto" className="picColmar" />
+                <img src={colmar} width="500" height="auto" className="picColmar"  />
                 <p className='colmarText'>Colmar Academy is the first project I have ever made. This project was part of the CodeAcademy's course 'Build Websites from Scratch', and its main goal was to practice my newly acquired HTML & CSS skills. I was provided with the website layouts for both desktop and mobile versions, while the styling was designed by me.</p>
+                <a href="https://github.com/peedans/peedans.github.io/tree/main/colmar-academy" target="_blank" rel="noopender noreferrer" className="github"><i className="fab fa-github"></i></a>
             </div>
             <div className='jamming'>
                 <h2 className='jammingHead'>jamming Project</h2>
-                <img src={jamming} width="500" height="auto"  className="picJamming"/>
+                <img src={jamming} width="500" height="auto"  className="picJamming" href=""/>
                 
                 <p className='jammingText'>In this project, I will build a React web application called Jammming. I will use my knowledge of React components, passing state, and requests with the Spotify API to build a website that allows users to search the Spotify library, create a custom playlist, then save it to their Spotify account.</p>
+                <a href="https://github.com/peedans/jsd-jamming-assessment" target="_blank" rel="noopender noreferrer" className="github"><i className="fab fa-github"></i></a>
             </div>
             <div className='Santa'>
                 <h2 className='santaHead'>Santa Project</h2>
                 <img src={Activity} width="500" height="auto" />
                 <p className='santaText'>I make a website about collecting exercise data such as calculating bmi, activity, location, etc. The front end website uses React, the backend uses Express to manage the RESTful API and finally Mongoose. to connect to MongoDB</p>
+                <a href="https://github.com/peedans/Santa-Frontend" target="_blank" rel="noopender noreferrer" className="github"><i className="fab fa-github"></i></a>
             </div>
         </div>
     )
